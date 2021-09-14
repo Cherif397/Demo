5f5054e9f03f80b725745827794283f06dabdf28
Cherif397 <79149952+Cherif397@users.noreply.github.com>
2021-09-14 16:10:21 +0200

    Version initiale

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './nutrition/header/header.component';
+import { FooterComponent } from './nutrition/footer/footer.component';
+import { ConnexionComponent } from './nutrition/connexion/connexion.component';
+import { InscriptionComponent } from './nutrition/inscription/inscription.component';
+import { HomeComponent } from './nutrition/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ConnexionComponent,
+    InscriptionComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
