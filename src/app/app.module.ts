b9606fb57582ef327e3796de5fe1d39b9b92e962
Cherif397 <79149952+Cherif397@users.noreply.github.com>
2021-09-21 14:35:46 +0200

    blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,17 @@ import { FooterComponent } from './nutrition/footer/footer.component';
 import { ConnexionComponent } from './nutrition/connexion/connexion.component';
 import { InscriptionComponent } from './nutrition/inscription/inscription.component';
 import { HomeComponent } from './nutrition/home/home.component';
+import { MdpOublieComponent } from './nutrition/mdp-oublie/mdp-oublie.component';
+import { DashboardComponent } from './nutrition/dashboard/dashboard.component';
+import { NavGuaucheComponent } from './nutrition/dashboard/nav-guauche/nav-guauche.component';
+import { PresentationComponent } from './nutrition/presentation/presentation.component';
+import { LieuComponent } from './nutrition/lieu/lieu.component';
+import { RecommandationComponent } from './nutrition/recommandation/recommandation.component';
+import { BarComponent } from './nutrition/bar/bar.component';
+import { ChatsComponent } from './nutrition/chats/chats.component';
+import { BlogComponent } from './nutrition/blog/blog.component';
+import { MonProfilPraticienComponent } from './nutrition/mon-profil-praticien/mon-profil-praticien.component';
+import { HoraireComponent } from './nutrition/horaire/horaire.component';
 
 @NgModule({
   declarations: [
@@ -17,13 +28,22 @@ import { HomeComponent } from './nutrition/home/home.component';
     FooterComponent,
     ConnexionComponent,
     InscriptionComponent,
-    HomeComponent
+    HomeComponent,
+    MdpOublieComponent,
+    DashboardComponent,
+    NavGuaucheComponent,
+    PresentationComponent,
+    LieuComponent,
+    RecommandationComponent,
+    BarComponent,
+    ChatsComponent,
+    BlogComponent,
+    MonProfilPraticienComponent,
+    HoraireComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
+  imports:[
     BrowserAnimationsModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
